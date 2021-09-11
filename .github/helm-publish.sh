666ef787f303f07e51be5f569edc5a1430889c17
rajdas98 <mail.rajdas@gmail.com>
2021-09-11 17:46:08 +0530

    adding helm publish
    Signed-off-by: rajdas98 <mail.rajdas@gmail.com>

--- .github/helm-publish.sh
+++ .github/helm-publish.sh
@@ -8,13 +8,12 @@ WORKING_DIRECTORY="$PWD/deploy/kubernetes/platform"
   echo "ERROR: Environment variable GITHUB_PAGES_REPO is required"
   exit 1
 }
-[ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
+[ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=helm-gh-pages
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/signoz-charts"
 [ -d "$HELM_CHARTS_SOURCE" ] || {
   echo "ERROR: Could not find Helm charts in $HELM_CHARTS_SOURCE"
   exit 1
 }
-[ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
 [ "$GITHUB_BRANCH" ] || {
   echo "ERROR: Environment variable $GITHUB_BRANCH is required"
   exit 1
@@ -23,23 +22,15 @@ WORKING_DIRECTORY="$PWD/deploy/kubernetes/platform"
 echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
-echo "HELM_VERSION=$HELM_VERSION"
 echo "$GITHUB_BRANCH=$GITHUB_BRANCH"
 
 echo '>> Prepare...'
 mkdir -p /tmp/helm/bin
 mkdir -p /tmp/helm/publish
-apt-get update
-apt-get install ca-certificates git openssh
+sudo apt-get update
+sudo apt-get install ca-certificates git
 
 echo '>> Installing Helm...'
-cd /tmp/helm/bin
-wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-chmod +x linux-amd64/helm
-alias helm=/tmp/helm/bin/linux-amd64/helm
-helm version -c
-helm init -c
 wget https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod +x get-helm-3
 ./get-helm-3
@@ -47,8 +38,6 @@ helm version -c
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
-mkdir -p "$HOME/.ssh"
-ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
 git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
 
 echo '>> Building charts...'
@@ -69,8 +58,8 @@ if [ "$GITHUB_BRANCH" != "main" ]; then
 fi
 
 echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
-git config user.email "signoz@users.noreply.github.com"
-git config user.name signoz
+git config user.email "mail.rajdas@gmail.com"
+git config user.name rajdas98
 git add .
 git status
 git commit -m "Published by GH-Action"
